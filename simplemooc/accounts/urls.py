49ca75e94880b69035a0d1279d5f915dc89c90fc
vitoria <vitoriarntrindade@gmail.com>
2024-07-06 17:04:56 -0400

    add rotas/views/templates para gerenciar conta de usuario, alterar senha etc

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
-from .views import register
+from .views import register, dashboard, edit, edit_password
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path("", dashboard, name='dashboard'),
     path("entrar/", LoginView.as_view(template_name='accounts/login.html'), name="login"),
     path("cadastro/", register, name="register"),
-    path("sair/", LogoutView.as_view(next_page='/'), name="logout")
+    path("sair/", LogoutView.as_view(next_page='/'), name="logout"),
+    path("editar-perfil", edit, name="edit"),
+    path("editar-senha", edit_password, name="edit_password")
 ]
